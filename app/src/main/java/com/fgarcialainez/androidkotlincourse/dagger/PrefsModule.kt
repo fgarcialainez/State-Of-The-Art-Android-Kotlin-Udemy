b107e595fae69a3dfc27574cfa528106a8ed4b85
Felix Garcia Lainez <fgarcialainez@gmail.com>
2020-04-02 08:59:48 +0200

    Commit missing changes related to the previous refactoring

--- app/src/main/java/com/fgarcialainez/androidkotlincourse/dagger/PrefsModule.kt
+++ app/src/main/java/com/fgarcialainez/androidkotlincourse/dagger/PrefsModule.kt
@@ -1,17 +1,35 @@
 package com.fgarcialainez.androidkotlincourse.dagger
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatActivity
 import com.fgarcialainez.androidkotlincourse.utils.SharedPreferencesHelper
 import dagger.Module
 import dagger.Provides
+import javax.inject.Qualifier
 import javax.inject.Singleton
+
+// Enable inject the same dependency using either
+// an application context or an activity context
+const val CONTEXT_APP = "Application Context"
+const val CONTEXT_ACTIVITY= "Activity Context"
+
+@Qualifier
+annotation class TypeOfContext(val type: String)
 
 @Module
 class PrefsModule {
 
     @Provides
     @Singleton
+    @TypeOfContext(CONTEXT_APP)
     fun provideSharedPreferences(app: Application): SharedPreferencesHelper {
         return SharedPreferencesHelper(app)
+    }
+
+    @Provides
+    @Singleton
+    @TypeOfContext(CONTEXT_ACTIVITY)
+    fun provideSharedPreferencesActivity(activity: AppCompatActivity): SharedPreferencesHelper {
+        return SharedPreferencesHelper(activity)
     }
 }
